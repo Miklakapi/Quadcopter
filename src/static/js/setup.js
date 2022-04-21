5af783e91a8552d319275804c58914883de92375
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2022-04-21 20:26:09 +0200

    web page

--- src/static/js/setup.js
+++ src/static/js/setup.js
@@ -1,45 +1,8 @@
-//Power
-$('.slider').on('input', function () {
-    var percent = $('.slider').val();
-    $('.power').html(percent + '%');
-
-    var value = 5 + (5 * percent / 100);
-    $.get('http://192.168.0.202:8000/power/' + value);
-});
-
-//Right
-$('.right').click(function () {
-	$.get('http://192.168.0.202:8000/direction/4');
-});
-
-//Left
-$('.left').click(function () {
-	$.get('http://192.168.0.202:8000/direction/3');
-});
-
-//Forward
-$('.forward').click(function () {
-	$.get('http://192.168.0.202:8000/direction/1');
-});
-
-//Backward
-$('.backward').click(function () {
-	$.get('http://192.168.0.202:8000/direction/2');
-});
-
-//Left rotation
-$('.leftRotation').click(function () {
-	$.get('http://192.168.0.202:8000/rotation/1');
-});
-
-//Right rotation
-$('.rightRotation').click(function () {
-	$.get('http://192.168.0.202:8000/rotation/2');
-});
-
-//Stay
-$('.dot').click(function () {
-	var percent = $('.slider').val();
-	var value = 5 + (5 * percent / 100);
-	$.get('http://192.168.0.202:8000/power/' + value);
+$(document).ready(function () {
+}).on('click', 'button', function () {
+	$.get('http://192.168.1.3:5000/direction/' + $(this).attr('class'));
+}).on('change', '.slider', function () {
+	let val = $(this).val();
+	$('.power').html(val + '%');
+	$.get('http://192.168.1.3:5000/power/' + (5 + (5 * val / 100)));
 });
