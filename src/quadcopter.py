3bf95d0ed6914df9a13f3c0140db0a214af54bfc
Miklakapi <kkmiklaszewski@gmail.com>
2022-06-04 19:47:10 +0000

    Early base code for a quadcopter

--- src/quadcopter.py
+++ src/quadcopter.py
@@ -1,10 +1,25 @@
 """Module to control quadcopter"""
 
-from xmlrpc.client import Boolean
+from argparse import Action
+from tkinter import LEFT
 import RPi.GPIO as GPIO
 from time import sleep
+from enum import Enum
 
 from file_reader import FileReader
+from accelerometer import Accelerometer
+from measurements_fix import MeasurementsFixer
+from timers import Clock
+
+#       ┌────┐   ┌────┐
+#       │ 01 │   │ 02 │
+#       └────┘   └────┘
+#             \ /
+#              X
+#             / \
+#       ┌────┐   ┌────┐
+#       │ 03 │   │ 04 │
+#       └────┘   └────┘
 
 
 class Quadcopter:
@@ -12,14 +27,41 @@ class Quadcopter:
     Class that manage the quacopter 
     """
 
-    motor_list = {}
+    class Action(Enum):
+        STAY = 0
+        FORWARD = 1
+        ROTATE_LEFT = 2
+        LEFT = 3
+        ROTATE_RIGHT = 4
+        RIGHT = 5
+        BACKWARD = 6
+
+    motor_dict: dict = {}
     """Dictionary of motors"""
 
-    led_list = {}
+    led_dict: dict = {}
     """Dictionary of leds"""
 
-    main_power = None
+    main_power: float = None
     """Main power of motors"""
+
+    accelerometer: Accelerometer = None
+    """Accelerometer"""
+
+    measure_fixer: MeasurementsFixer = None
+    """Measure fixer"""
+
+    led_clock: Clock = None
+    """Led clock"""
+
+    action: Action = None
+    """Quadcopter action"""
+
+    x_angle_range: list = []
+    """X axis range"""
+
+    y_angle_range: list = []
+    """Y axis range"""
 
     def __init__(self) -> None:
         """
@@ -33,7 +75,7 @@ class Quadcopter:
 
         for x in motors:
             GPIO.setup(motors[x], GPIO.OUT, initial=GPIO.LOW)
-            self.motor_list.update({
+            self.motor_dict.update({
                 x: {
                     'pin': motors[x],
                     'gpio': GPIO.PWM(motors[x], 50),
@@ -43,14 +85,21 @@ class Quadcopter:
 
         for x in leds:
             GPIO.setup(leds[x], GPIO.OUT, initial=GPIO.LOW)
-            self.led_list.update({
+            self.led_dict.update({
                 x: {
                     'pin': leds[x],
                     'active': False
                 }
             })
 
+        self.accelerometer = Accelerometer()
+        self.measure_fixer = MeasurementsFixer()
+        self.led_clock = Clock()
+
+        self.set_action(self.Action(0))
+
         self.start_pwm()
+        self.start_leds()
 
     def set_main_power(self, main_power: float) -> None:
         """
@@ -63,14 +112,43 @@ class Quadcopter:
             raise Exception('Wrong power')
         self.main_power = main_power
 
-        for x in self.motor_list.values():
+        for x in self.motor_dict.values():
             x['gpio'].ChangeDutyCycle(main_power + x['extra_power'])
 
-    def set_action(self):
-        pass
+    def set_action(self, action: Action) -> None:
+        """
+        This method sets the activity of the quadcopter and changes the range of angles.
+        
+        :param action: Action | Drone action
+        :return: None
+        """
+        self.action = action
+        self.x_angle_range = [-1, 1]
+        self.y_angle_range = [-1, 1]
+        if action == self.Action.FORWARD:
+            self.x_angle_range = [18, 22]
+        elif action == self.Action.BACKWARD:
+            self.x_angle_range = [-18, -22]
+        elif action == self.Action.LEFT:
+            self.y_angle_range = [-18, -22]
+        elif action == self.Action.RIGHT:
+            self.y_angle_range = [18, 22]
 
-    def run(self):
-        pass
+    def run(self) -> None:
+        """
+        This method manages the quadcopter using data from the accelerometer.
+
+        :return: None
+        """
+        if self.led_clock.get_elapsed_time().as_seconds() >= 2:
+            for x in self.led_dict.values():
+                self.switch_led(x)
+            self.led_clock.restart()
+        
+        for i in range(3):
+            self.measure_fixer.add_measurement(self.accelerometer.run())
+            sleep(0.06)
+        angle = self.measure_fixer.get_fixed_measurement()
 
     def start_pwm(self) -> None:
         """
@@ -78,23 +156,51 @@ class Quadcopter:
 
         :return: None
         """
-        for x in self.motor_list:
-            self.motor_list[x]['gpio'].start(4)
+        for x in self.motor_dict.values():
+            x['gpio'].start(4)
 
         sleep(5)
-        for x in self.motor_list:
-            self.motor_list[x]['gpio'].ChangeDutyCycle(5.7)
-            self.switch_led(self.led_list[x])
+        for x in self.motor_dict:
+            self.motor_dict[x]['gpio'].ChangeDutyCycle(5.7)
+            self.set_led(self.led_dict[x], True)
             sleep(2)
-            self.motor_list[x]['gpio'].ChangeDutyCycle(5)
+            self.motor_dict[x]['gpio'].ChangeDutyCycle(5)
+            self.set_led(self.led_dict[x], False)
         self.main_power = 5
+    
+    def start_leds(self) -> None:
+        """
+        This method informs you that the drone is ready by means of LEDs. It also sets the appropriate state of the LEDs and resets the clock.
 
-    def switch_led(led: dict) -> Boolean:
+        :return: None
+        """
+        for x in range(3):
+            for x in self.led_dict.values():
+                self.set_led(x, True)
+            sleep(0.3)
+            for x in self.led_dict.values():
+                self.set_led(x, False)
+
+        self.set_led(self.led_dict['frontLeft'], True)
+        self.set_led(self.led_dict['backRigth'], True)
+
+    def set_led(led: dict, active: bool) -> None:
+        """
+        This method set led state
+
+        :param led: dict | Led
+        :param active: bool | Led status
+        :return: None
+        """
+        GPIO.setup(led['pin'], active)
+        led['active'] = active
+
+    def switch_led(led: dict) -> bool:
         """
         This method changes the state of the led.
         
         :param led: dict | Led
-        :return: Boolean | Led state
+        :return: bool | Led state
         """
         led['active'] = not led['active']
         GPIO.setup(led['pin'], led['active'])
@@ -106,11 +212,11 @@ class Quadcopter:
 
         :return: None
         """
-        for x in self.motor_list.values():
+        for x in self.motor_dict.values():
             x['gpio'].ChangeDustyCycle(5)
         sleep(0.5)
-        for x in self.motor_list.values():
+        for x in self.motor_dict.values():
             x.stop()
-        for x in self.led_list.values():
+        for x in self.led_dict.values():
             GPIO.setup(x['pin'], 0)
         GPIO.cleanup()
