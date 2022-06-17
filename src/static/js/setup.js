83972de9f6df8f0b8ce699eeb1e8f3cbeca877cd
Miklakapi <kkmiklaszewski@gmail.com>
2022-06-17 21:18:53 +0000

    Control interface base

--- src/static/js/setup.js
+++ src/static/js/setup.js
@@ -1,8 +1,11 @@
+const socket = io.connect('http://192.168.1.185:5000');
+
 $(document).ready(function () {
+
 }).on('click', 'button', function () {
-	$.get('http://192.168.1.185:5000/direction/' + $(this).attr('class'));
+	socket.send($(this).attr('class')); //TODO
 }).on('change', '.slider', function () {
 	let val = $(this).val();
-	$('.power').html(val + '%');
-	$.get('http://192.168.1.185:5000/power/' + (5 + (5 * val / 100)));
+	$('.power').html(val + ' %');
+    socket.send((5 + (5 * val / 100)).toFixed(2)); //TODO
 });
